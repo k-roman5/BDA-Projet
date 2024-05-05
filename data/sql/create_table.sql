0113f55b4dc45916bc14b302c81d29f5fad91ccf
romank2 <102798439+k-roman5@users.noreply.github.com>
2024-05-05 20:53:38 +0200

    Ajout de DROP CASCADE

--- data/sql/create_table.sql
+++ data/sql/create_table.sql
@@ -1,26 +1,26 @@
 CREATE TABLE regions (
-    reg int constraint cle_reg PRIMARY KEY,
+    reg int CONSTRAINT cle_reg PRIMARY KEY,
     libelle varchar(30)
 );
 
 CREATE TABLE departements (
-    dep varchar(10) constraint cle_dep PRIMARY KEY,
-    reg int constraint cle_reg_departements references regions(reg),
+    dep varchar(10) CONSTRAINT cle_dep PRIMARY KEY,
+    reg int CONSTRAINT cle_reg_departements REFERENCES regions(reg),
     libelle varchar(30)
 );
 
 CREATE TABLE communes (
-    com varchar(10) constraint cle_com PRIMARY KEY,
-    dep varchar(10) constraint cle_dep_communes references departements(dep),
+    com varchar(10) CONSTRAINT cle_com PRIMARY KEY,
+    dep varchar(10) CONSTRAINT cle_dep_communes REFERENCES departements(dep),
     libelle varchar(50)
 );
 
 CREATE TABLE cheflieureg (
-    reg int constraint cle_reg_cheflieureg references regions(reg),
-    com varchar(10) constraint cle_com_cheflieureg references communes(com)
+    reg int CONSTRAINT cle_reg_cheflieureg REFERENCES regions(reg),
+    com varchar(10) CONSTRAINT cle_com_cheflieureg REFERENCES communes(com)
 );
 
 CREATE TABLE cheflieudep (
-    dep varchar(10) constraint cle_dep_cheflieudep references departements(dep),
-    com varchar(10) constraint cle_com_cheflieudep references communes(com)
+    dep varchar(10) CONSTRAINT cle_dep_cheflieudep REFERENCES departements(dep),
+    com varchar(10) CONSTRAINT cle_com_cheflieudep REFERENCES communes(com)
 );
