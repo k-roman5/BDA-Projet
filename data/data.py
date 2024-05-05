0b34d2c14571c7163020c6f4a2617642e745ece4
romank2 <102798439+k-roman5@users.noreply.github.com>
2024-05-05 17:20:07 +0200

    Réorganisation des fichiers

--- data/data.py
+++ data/data.py
@@ -1,20 +1,31 @@
 import psycopg2
+import csv
 
 dbname = "postgres"
 user = "postgres"
 password = "SQL"
-host = "localhost" 
+host = "localhost"
 port = "5432"
+
+csv_file = "data/datafiles/area/v_commune_2023.csv"
 
 try:
     connection = psycopg2.connect(
         dbname = dbname, user = user, password = password, host = host, port = port
     )
-    print("Connexion réussie ! Yippee !")
-    
-    
-    connection.close()
-    print("Connexion fermée.")
-    
+    cursor = connection.cursor()
+
+    with open(csv_file, 'r') as f:
+        next(f)
+        cursor.copy_from(f, 'communities', sep=',')
+
+    connection.commit()
+    print("Données importées avec succès !")
+
 except psycopg2.Error as e:
-    print("Erreur lors de la connexion à la base de données :", e)
+    print("Erreur lors de l'import des données dans la base de données :", e)
+
+finally:
+    if connection:
+        cursor.close()
+        connection.close()
