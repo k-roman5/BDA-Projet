a3bb51a1481e495e73f4815a346ac535471276f9
romank2 <102798439+k-roman5@users.noreply.github.com>
2024-05-06 02:07:14 +0200

    Update create_db.py

--- data/create_db.py
+++ data/create_db.py
@@ -4,20 +4,18 @@ from connect_db import connection
 script_path = "data/sql/create_table.sql"
 
 try:
-    cursor = connection.cursor()
+    with connection.cursor() as cursor:
+        with open(script_path, 'r') as file:
+            sql_script = file.read()
 
-    with open(script_path, 'r') as file:
-        sql_script = file.read()
+        cursor.execute(sql_script)
+        connection.commit()
+        print("Tables created successfully.")
 
-    cursor.execute(sql_script)
-    connection.commit()
-    print("Tables created successfully.")
-
-except (Exception, Error) as error:
-    print("Error creating tables :", error)
+except Error as error:
+    print("Error creating tables:", error)
 
 finally:
     if connection:
-        cursor.close()
         connection.close()
         print("Database connection closed.")
