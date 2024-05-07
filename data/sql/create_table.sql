46967866f357a7ae90533b5ce2791b10ffe5caa5
romank2 <102798439+k-roman5@users.noreply.github.com>
2024-05-07 20:24:09 +0200

    Creation de la table de population

--- data/sql/create_table.sql
+++ data/sql/create_table.sql
@@ -1,41 +1,43 @@
 CREATE TABLE regions (
-    reg varchar(10) CONSTRAINT cle_reg PRIMARY KEY,
-    libelle varchar(30)
+    code_reg VARCHAR(10) CONSTRAINT cle_reg PRIMARY KEY,
+    nom VARCHAR(30)
 );
 
 CREATE TABLE departements (
-    dep varchar(10) CONSTRAINT cle_dep PRIMARY KEY,
-    reg varchar(10) CONSTRAINT cle_reg_departements REFERENCES regions(reg),
-    libelle varchar(30)
+    code_dep VARCHAR(10) CONSTRAINT cle_dep PRIMARY KEY,
+    code_reg VARCHAR(10) CONSTRAINT cle_reg_departements REFERENCES regions(code_reg),
+    nom VARCHAR(30)
 );
 
 CREATE TABLE communes (
-    com varchar(10) CONSTRAINT cle_com PRIMARY KEY,
-    dep varchar(10) CONSTRAINT cle_dep_communes REFERENCES departements(dep),
-    libelle varchar(50)
+    code_com VARCHAR(10) CONSTRAINT cle_com PRIMARY KEY,
+    code_dep VARCHAR(10) CONSTRAINT cle_dep_communes REFERENCES departements(code_dep),
+    nom VARCHAR(50)
 );
 
 CREATE TABLE cheflieureg (
-    reg varchar(10) CONSTRAINT cle_reg_cheflieureg REFERENCES regions(reg),
-    com varchar(10) CONSTRAINT cle_com_cheflieureg REFERENCES communes(com)
+    code_reg VARCHAR(10) CONSTRAINT cle_reg_cheflieureg REFERENCES regions(code_reg),
+    code_com VARCHAR(10) CONSTRAINT cle_com_cheflieureg REFERENCES communes(code_com)
 );
 
 CREATE TABLE cheflieudep (
-    dep varchar(10) CONSTRAINT cle_dep_cheflieudep REFERENCES departements(dep),
-    com varchar(10) CONSTRAINT cle_com_cheflieudep REFERENCES communes(com)
+    code_dep VARCHAR(10) CONSTRAINT cle_dep_cheflieudep REFERENCES departements(code_dep),
+    code_com VARCHAR(10) CONSTRAINT cle_com_cheflieudep REFERENCES communes(code_com)
 );
 
+-- Tables pour les données de population
+
 CREATE TABLE populations (
-    com VARCHAR(10) CONSTRAINT cle_com_populations REFERENCES communes(com), 
-    pop2020 INT CONSTRAINT non_neg_pop2020 CHECK (pop2020 >= 0),
-    pop2014 INT CONSTRAINT non_neg_pop2014 CHECK (pop2014 >= 0),
-    pop2009 INT CONSTRAINT non_neg_pop2009 CHECK (pop2009 >= 0),
-    pop1999 INT CONSTRAINT non_neg_pop1999 CHECK (pop1999 >= 0),
-    pop1990 INT CONSTRAINT non_neg_pop1990 CHECK (pop1990 >= 0),
-    pop1982 INT CONSTRAINT non_neg_pop1982 CHECK (pop1982 >= 0),
-    pop1975 INT CONSTRAINT non_neg_pop1975 CHECK (pop1975 >= 0),
-    pop1968 INT CONSTRAINT non_neg_pop1968 CHECK (pop1968 >= 0)
+    code_com VARCHAR(10) NOT NULL,
+    anneedebut INT NOT NULL,
+    anneefin INT NOT NULL,
+    type_stat VARCHAR(20) NOT NULL,
+    valeur_stat FLOAT,
+    CONSTRAINT pk_populations PRIMARY KEY (code_com, anneedebut, anneefin, type_stat),
+    CONSTRAINT fk_populations_communes FOREIGN KEY (code_com) REFERENCES communes(code_com)
 );
+
+--
 
 
 -- Tables pour les données de mariage
