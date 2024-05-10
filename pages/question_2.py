0e8b62e4b5a87262770963d57415feba59dfb394
romank2 <102798439+k-roman5@users.noreply.github.com>
2024-05-10 03:33:36 +0200

    Question 2

--- pages/question_2.py
+++ pages/question_2.py
@@ -2,17 +2,18 @@ import dash
 import dash_bootstrap_components as dbc
 import dash_mantine_components as dmc
 from dash import html, callback, Output, Input, dcc
-from view.question2 import view_of_the_view, years
+from view.question2 import view_of_the_view1, view_of_the_view2, years1
 
 dash.register_page(__name__, location="sidebar", external_stylesheets=[
     dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
 
-query = """
+query1 = """
 CREATE OR REPLACE VIEW population_departements AS 
-    SELECT d.nom AS nom_departement, d.code_dep AS code_departement, p.annee_debut, p.annee_fin, SUM(p.valeur_stat) AS populations
+    SELECT d.nom AS nom_departement, d.code_dep AS code_departement, p.annee_debut, SUM(p.valeur_stat) AS populations
     FROM departements d
     JOIN communes c ON d.code_dep = c.code_dep
     JOIN populations p ON c.code_com = p.code_com
+    WHERE p.type_stat = 'population'
     GROUP BY d.nom, d.code_dep, p.annee_debut, p.annee_fin;
 """
 
@@ -27,18 +28,18 @@ def layout():
         dbc.Card([
             dbc.CardHeader(html.Span("Requête SQL", style={'color': 'black'})),
             dbc.CardBody([
-                html.Pre(query, className="mb-0", style={'color': 'black', 'overflowY': 'scroll', 'max-height': '200px'}),
+                html.Pre(query1, className="mb-0", style={'color': 'black', 'overflowY': 'scroll', 'max-height': '200px'}),
             ], style={'padding': '10px'}),
         ], color="light", inverse=True),
         html.Br(),
         html.Div([
             html.Label("Sélectionnez une année :"),
             dmc.Select(placeholder="Choissez une années",
-                                id="year-dropdown",
-                                data=years().rename(columns={'annee_debut': 'value'}).to_dict(orient='records'),
+                                id="year-dropdown1",
+                                data=years1().rename(columns={'annee_debut': 'value'}).to_dict(orient='records'),
                                 value='2020'
                                 ),
-            html.Div(view_of_the_view('2020'), id='view-table', style={'overflowY': 'scroll', 'height': '400px'})
+            html.Div(view_of_the_view1('2020'), id='view-table1', style={'overflowY': 'scroll', 'height': '400px'})
         ]),
         html.Br(),
         html.Br(),
@@ -46,9 +47,19 @@ def layout():
     ])
 
 @callback(
-    Output('view-table', 'children'),
-    Input('year-dropdown', 'value')
+    Output('view-table1', 'children'),
+    Input('year-dropdown1', 'value'),
+    prevent_initial_call = True
 )
-def update_view(year):
-    updated_view = view_of_the_view(year)
+def update_view1(year):
+    updated_view = view_of_the_view1(year)
+    return updated_view
+
+@callback(
+    Output('view-table2', 'children'),
+    Input('year-dropdown2', 'value'),
+    prevent_initial_call = True
+)
+def update_view2(year):
+    updated_view = view_of_the_view2(year)
     return updated_view
--- view/question2.py
+++ view/question2.py
@@ -11,21 +11,44 @@ def df_to_table(df):
         dbc.Table.from_dataframe(df, striped=True, bordered=True, hover=True, className='table-sm table-md')
     ])
     
-query = """
+query1 = """
 CREATE OR REPLACE VIEW population_departements AS 
-    SELECT d.nom AS nom_departement, d.code_dep AS code_departement, p.annee_debut, p.annee_fin, SUM(p.valeur_stat) AS populations
+    SELECT d.nom AS nom_departement, d.code_dep AS code_departement, p.annee_debut, SUM(p.valeur_stat) AS populations
     FROM departements d
     JOIN communes c ON d.code_dep = c.code_dep
     JOIN populations p ON c.code_com = p.code_com
+    WHERE p.type_stat = 'population'
     GROUP BY d.nom, d.code_dep, p.annee_debut, p.annee_fin;
 """
 
-def view_of_the_view(year=None):
-    create_or_replace_view(query)
+query2 = """
+CREATE OR REPLACE VIEW population_regions AS 
+    SELECT r.nom AS nom_regions, r.code_reg AS code_regions, p.annee_debut, SUM(p.valeur_stat) AS populations
+    FROM regions r
+    JOIN departements d ON r.code_reg = d.code_reg
+    JOIN communes c ON d.code_dep = c.code_dep
+    JOIN populations p ON c.code_com = p.code_com
+    WHERE p.type_stat = 'population'
+    GROUP BY r.nom, r.code_reg, p.annee_debut, p.annee_fin;
+"""
+
+def view_of_the_view1(year=None):
+    create_or_replace_view(query1)
     query_result = query_to_df(f"SELECT * FROM population_departements WHERE annee_debut = '{year}'")
     return df_to_table(query_result)
 
-def years():
+def view_of_the_view2(year=None):
+    create_or_replace_view(query2)
+    query_result = query_to_df(f"SELECT * FROM population_regions WHERE annee_debut = '{year}'")
+    return df_to_table(query_result)
+
+
+def years1():
     query = "SELECT DISTINCT annee_debut FROM populations;"
+    df = query_to_df(query)
+    return df
+
+def years2():
+    query = "SELECT DISTINCT annee_fin FROM populations;"
     df = query_to_df(query)
     return df
